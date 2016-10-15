de53dd5b98a487ff83e3c5baf8eba04adc726f45
stewart <stewart@snwebdev.com>
2016-10-15 08:39:48 +0100

    add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,14 @@ var redux = require('redux');
 
 console.log('starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+};
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
   // state = state || {name: 'Anonymous'};
 
 
@@ -12,6 +19,39 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
     return {
       ...state,
       name: action.name
+    };
+    case 'ADD_HOBBY':
+    return {
+      ...state,
+      hobbies: [
+        ...state.hobbies,
+        {
+          id: nextHobbyId++,
+          hobby: action.hobby
+        }
+      ]
+    };
+    case 'REMOVE_HOBBY':
+    return {
+      ...state,
+      hobbies:  state.hobbies.filter((hobby) => hobby.id !== action.id)
+    };
+    case 'ADD_MOVIE':
+    return {
+      ...state,
+      movies: [
+        ...state.movies,
+        {
+          id: nextMovieId++,
+          title: action.title,
+          gendre: action.gendre
+        }
+      ]
+    };
+    case 'REMOVE_MOVIE':
+    return {
+      ...state,
+      movieies:  state.movies.filter((movie) => movie.id !== action.id)
     };
     default:
     return state;
@@ -25,6 +65,7 @@ var unsubscribe = store.subscribe(() => {
 
   console.log('Name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+  console.log('New state', store.getState());
 });
 // unsubscribe();
 
@@ -36,9 +77,43 @@ store.dispatch({
   name: 'Stewart'
 });
 
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'running'
+});
 
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'walking'
+});
+
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
 
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Bob'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  movie: {
+    title: "The Sound of music",
+    gendre: 'Musical'
+  }
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: "Kess",
+  gendre: 'Drama'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
 });
